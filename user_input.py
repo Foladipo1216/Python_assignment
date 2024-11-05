2539f192c0bb843aa949a32edb03fac8ff081fba
Foladipo1216 <oladosufolashade@gmail.com>
2024-11-05 15:34:39 +0100

    input

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name?")
+age = int(input("Enter your age:"))
+location = input("Where do you live?")
+
+print(f"{name} you are {age} years old and you live in {location}")
